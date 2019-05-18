be88012a1c5fbaa2243d5eb877c46967bce4ac19
Marcin Malessa <marcin@malessa.pl>
2019-05-18 19:43:08 +0200

    Added: broadway/event-store-dbal

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Broadway\Bundle\BroadwayBundle\BroadwayBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
 ];
---
+++ src/UI/Command/InitDbalEventStoreCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\UI\Command;
+
+use Broadway\EventStore\Dbal\DBALEventStore;
+use Doctrine\DBAL\Connection;
+use Exception;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InitDbalEventStoreCommand extends Command
+{
+    protected static $defaultName = 'app:eventstore:schemainit';
+    private $connection;
+    private $eventStore;
+
+    public function __construct(
+        Connection $connection,
+        DBALEventStore $eventStore
+    ) {
+        $this->connection = $connection;
+        $this->eventStore = $eventStore;
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('Init DBAL event store');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if (!$this->connection) {
+            $output->writeln('<error>Could not create Broadway event-store schema</error>');
+            $output->writeln(sprintf('<error>%s</error>', $this->exception->getMessage()));
+            return 1;
+        }
+
+        $error = false;
+        try {
+            $schemaManager = $this->connection->getSchemaManager();
+            $schema = $schemaManager->createSchema();
+            $eventStore = $this->eventStore;
+
+            $table = $eventStore->configureSchema($schema);
+            if (null !== $table) {
+                $schemaManager->createTable($table);
+                $output->writeln('<info>Created Broadway event-store schema</info>');
+            } else {
+                $output->writeln('<info>Broadway event-store schema already exists</info>');
+            }
+        } catch (Exception $e) {
+            $output->writeln('<error>Could not create Broadway event-store schema</error>');
+            $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
+            $error = true;
+        }
+
+        return $error ? 1 : 0;
+    }
+}
+
+
